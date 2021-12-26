e3f5a30dab14aa396ba2102ae41ae5f5617fbafe
Sebastian Gaete <locksdanny@outlook.com>
2021-12-26 17:50:12 -0300

    show regalos count on amigo card

--- navidad_dam/lib/widgets/amigo_card.dart
+++ navidad_dam/lib/widgets/amigo_card.dart
@@ -1,10 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coolicons/coolicons.dart';
 import 'package:flutter/material.dart';
 import 'package:navidad_dam/pages/amigo_page.dart';
+import 'package:navidad_dam/service/firestore_service.dart';
 import 'package:navidad_dam/util/nav.dart';
 
 class AmigoCard extends StatelessWidget {
   final String id;
+  final String usuarioUID;
   final String nombre;
   final String email;
   final String descripcion;
@@ -12,6 +15,7 @@ class AmigoCard extends StatelessWidget {
 
   AmigoCard(
       {this.id = 'default',
+      this.usuarioUID = 'default',
       this.nombre = 'default',
       this.email = 'default',
       this.descripcion = 'default',
@@ -19,17 +23,31 @@ class AmigoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => NavUtil.navigateTo(
-          context, AmigoDetail(id, nombre, email, descripcion, profesion)),
-      child: Card(
-        child: ListTile(
-          leading: const Icon(Coolicons.user),
-          title: Text(nombre),
-          subtitle: Text(profesion),
-          trailing: Text('Regalos: '),
-        ),
-      ),
+    return StreamBuilder(
+      stream: FirestoreService().regalos(id),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData ||
+            snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        int cantidad = snapshot.data!.docs.length;
+
+        return GestureDetector(
+          onTap: () => NavUtil.navigateTo(
+              context,
+              AmigoDetail(
+                  id, usuarioUID, nombre, email, descripcion, profesion)),
+          child: Card(
+              child: ListTile(
+                  leading: const Icon(Coolicons.user),
+                  title: Text(nombre),
+                  subtitle: Text(profesion),
+                  trailing: Text('Regalos: ${cantidad}'))),
+        );
+      },
     );
   }
 }
