383bb9b9df0b6ee94a17f14afbb2f7665c3be131
Sebastian Gaete <locksdanny@outlook.com>
2021-12-26 15:02:05 -0300

    successful login & signup validation and functionality

--- navidad_dam/lib/pages/form_login.dart
+++ navidad_dam/lib/pages/form_login.dart
@@ -1,6 +1,7 @@
 import 'package:coolicons/coolicons.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:navidad_dam/constants.dart';
 import 'package:navidad_dam/pages/user_dashboard.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -8,50 +9,35 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 import 'package:navidad_dam/util/nav.dart';
 
 class FormLogin extends StatefulWidget {
+  FormLogin({Key? key}) : super(key: key);
+
   @override
   _FormLoginState createState() => _FormLoginState();
 }
 
 class _FormLoginState extends State<FormLogin> {
+  final formKey = GlobalKey<FormState>();
   TextEditingController emailCtrl = TextEditingController();
   TextEditingController passwordCtrl = TextEditingController();
   String errorText = '';
-  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(15),
         child: Form(
+          key: formKey,
           child: Column(
             children: [
-              TextFormField(
-                  controller: emailCtrl,
-                  decoration: InputDecoration(
-                      labelText: 'Email', icon: Icon(MdiIcons.cardAccountMail)),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (email) {
-                    if (email!.isEmpty) {
-                      return 'Ingrese Email';
-                    } else {
-                      return null;
-                    }
-                  }),
-              TextFormField(
-                  controller: passwordCtrl,
-                  decoration: InputDecoration(
-                      labelText: 'Contraseña', icon: Icon(MdiIcons.accountKey)),
-                  obscureText: true,
-                  validator: (clave) {
-                    if (clave!.isEmpty) {
-                      return 'Ingrese descripción de su amigo';
-                    } else {
-                      return null;
-                    }
-                  }),
+              campoEmail(),
+              campoClave(),
               botonLogin(),
               Container(
-                child: Text(errorText),
+                child: Text(
+                  errorText,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
               )
             ],
           ),
@@ -60,7 +46,37 @@ class _FormLoginState extends State<FormLogin> {
     );
   }
 
-  Container botonLogin() {
+  TextFormField campoEmail() {
+    return TextFormField(
+        controller: emailCtrl,
+        decoration: InputDecoration(
+            labelText: 'Email', icon: Icon(MdiIcons.cardAccountMail)),
+        keyboardType: TextInputType.emailAddress,
+        validator: (email) {
+          if (email!.isEmpty) {
+            return 'Ingrese Email';
+          } else {
+            return null;
+          }
+        });
+  }
+
+  TextFormField campoClave() {
+    return TextFormField(
+        controller: passwordCtrl,
+        decoration: InputDecoration(
+            labelText: 'Contraseña', icon: Icon(MdiIcons.accountKey)),
+        obscureText: true,
+        validator: (clave) {
+          if (clave!.isEmpty) {
+            return 'Debe ingresar una contraseña';
+          } else {
+            return null;
+          }
+        });
+  }
+
+  Widget botonLogin() {
     return Container(
       margin: EdgeInsets.all(10),
       width: double.infinity,
@@ -68,7 +84,7 @@ class _FormLoginState extends State<FormLogin> {
         child: Text('Iniciar Sesion'),
         onPressed: () async {
           if (formKey.currentState!.validate()) {
-            print('Registrado');
+            print('Signed in');
           }
           UserCredential? userCredencial;
           try {
@@ -88,8 +104,14 @@ class _FormLoginState extends State<FormLogin> {
               case 'wrong-password':
                 errorText = 'Contraseña Incorrecta';
                 break;
+              case 'invalid-password':
+                errorText = 'La contraseña ingrsada no es valida';
+                break;
+              case 'invalid-email':
+                errorText = 'El email ingresado no es valido';
+                break;
               default:
-                errorText = 'Error: ' + except.code;
+                print('Error ${except.code}');
             }
             setState(() {});
           }
--- navidad_dam/lib/pages/form_registro.dart
+++ navidad_dam/lib/pages/form_registro.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:navidad_dam/constants.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:navidad_dam/pages/user_dashboard.dart';
+import 'package:navidad_dam/util/nav.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class form_registro extends StatefulWidget {
   form_registro({Key? key}) : super(key: key);
@@ -13,6 +17,11 @@ class _form_registroState extends State<form_registro> {
   final formKey = GlobalKey<FormState>();
   final correoValidator =
       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+
+  TextEditingController emailCtrl = TextEditingController();
+  TextEditingController passwordCtrl = TextEditingController();
+  String errorText = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,12 +32,19 @@ class _form_registroState extends State<form_registro> {
             Container(
               child: Expanded(
                 child: Padding(
-                  padding: EdgeInsets.all(5),
+                  padding: EdgeInsets.all(15),
                   child: ListView(
                     children: [
                       campoEmail(),
                       campoClave(),
                       botonRegistrar(),
+                      Container(
+                        child: Text(
+                          errorText,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      )
                     ],
                   ),
                 ),
@@ -42,6 +58,7 @@ class _form_registroState extends State<form_registro> {
 
   TextFormField campoClave() {
     return TextFormField(
+      controller: passwordCtrl,
       decoration: InputDecoration(
           labelText: 'Contraseña', icon: Icon(MdiIcons.accountKey)),
       validator: (clave) {
@@ -56,8 +73,9 @@ class _form_registroState extends State<form_registro> {
 
   TextFormField campoEmail() {
     return TextFormField(
+      controller: emailCtrl,
       decoration: InputDecoration(
-          labelText: 'Correo', icon: Icon(MdiIcons.cardAccountMail)),
+          labelText: 'Email', icon: Icon(MdiIcons.cardAccountMail)),
       keyboardType: TextInputType.emailAddress,
       validator: (email) {
         if (email!.isEmpty) {
@@ -77,9 +95,36 @@ class _form_registroState extends State<form_registro> {
       padding: EdgeInsets.all(5),
       child: ElevatedButton(
         child: Text('Registrar'),
-        onPressed: () {
+        onPressed: () async {
           if (formKey.currentState!.validate()) {
             print('Registrado');
+          }
+          UserCredential? userCredential;
+          try {
+            userCredential = await FirebaseAuth.instance
+                .createUserWithEmailAndPassword(
+                    email: emailCtrl.text.trim(),
+                    password: passwordCtrl.text.trim());
+            SharedPreferences sp = await SharedPreferences.getInstance();
+            sp.setString('user_email', emailCtrl.text.trim());
+
+            NavUtil.navigateTo(context, UserDashboard(), replacement: true);
+          } on FirebaseAuthException catch (except) {
+            switch (except.code) {
+              case 'invalid-email':
+                errorText = 'El correo ingresado es invalido';
+                break;
+              case 'weak-password':
+                errorText =
+                    'La contraseña ingresada es muy debil, por favor agregar mas caracteres y numeros';
+                break;
+              case 'email-already-in-use':
+                errorText = 'Ya existe un usuario con ese correo';
+                break;
+              default:
+                print(except.code);
+            }
+            setState(() {});
           }
         },
       ),
